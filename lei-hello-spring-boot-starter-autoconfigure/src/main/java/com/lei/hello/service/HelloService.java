078d2190f3fd7d56ea64864122065a85c80f916e
yunlonglei <yunlonglei0@gmail.com>
2021-08-10 15:58:28 +0800

    update

--- lei-hello-spring-boot-starter-autoconfigure/src/main/java/com/lei/hello/service/HelloService.java
+++ lei-hello-spring-boot-starter-autoconfigure/src/main/java/com/lei/hello/service/HelloService.java
@@ -11,10 +11,33 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @since 2021/8/10 3:12 下午
  */
 public class HelloService {
+    private String name = "lei";
+    private Integer age = 26;
+
     @Autowired
     Helloproperties helloproperties;
 
     public String sayHello(String name) {
         return helloproperties.getPrefix() + ":" + name + "," + helloproperties.getSuffix();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String sayHelloToo() {
+        return "我叫" + name + "，" + "今年" + age;
     }
 }
